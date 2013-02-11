4b538720b68efcc0060677b9a3363c21791f5a6f
CheezeCake <emmanuel.nicolet@gmail.com>
2013-02-11 12:59:33 +0100

    first commit

---
+++ entier.cpp
@@ -0,0 +1,139 @@
+#include "entier.hpp"
+
+using namespace std;
+
+Entier::Entier()
+{
+}
+
+Entier::Entier(unsigned int val)
+{
+    valeur.push_back(val);
+}
+
+Entier::Entier(const string& val)
+{
+    valeur.push_back(0);
+
+    /*const unsigned int BASE=10;
+    for(unsigned int i = 0; i < val.size(); i++)
+    {
+        valeur*=BASE;
+        valeur+=val[i] - '0';
+    }*/
+}
+
+Entier::Entier(const Entier& val)
+{
+    valeur = val.valeur;
+}
+
+Entier::~Entier()
+{
+}
+
+void Entier::operator=(const Entier& b)
+{
+    valeur = b.valeur;
+}
+
+Entier &Entier::operator+=(const Entier& b)
+{
+    return *this;
+}
+
+Entier &Entier::operator-=(const Entier& b)
+{
+    return *this;
+}
+
+Entier &Entier::operator*=(const Entier& b)
+{
+    return *this;
+}
+
+bool operator<(const Entier& a, const Entier& b)
+{
+    return true;
+}
+
+bool operator>(const Entier& a, const Entier &b)
+{
+    return true;
+}
+
+bool Entier::estEgal(const Entier& b) const
+{
+    return true;
+}
+
+bool operator==(const Entier& a, const Entier& b)
+{
+    return a.estEgal(b);
+}
+
+bool operator!=(const Entier& a, const Entier& b)
+{
+    if (a == b)
+        return false;
+    return true;
+    //return !(a == b);
+}
+
+bool operator<=(const Entier& a, const Entier& b)
+{
+    return true;
+}
+
+bool operator>=(const Entier& a, const Entier& b)
+{
+    return true;
+}
+
+Entier operator+(const Entier& a, const Entier& b)
+{
+    Entier somme;
+
+    return somme;
+}
+
+Entier operator-(const Entier& a, const Entier& b)
+{
+    Entier soustraction;
+
+    return soustraction;
+}
+
+Entier operator*(const Entier& a, const Entier& b)
+{
+    Entier produit;
+
+    return produit;
+}
+
+ostream& operator<<(ostream& flux, const Entier& val)
+{
+    for(unsigned int i=val.valeur.size()-1; i!=0; i--)
+    {
+        flux<<val.valeur[i]<<' ';
+    }
+
+    return flux;
+}
+
+istream& operator>>(istream& flux, Entier& val)
+{
+    //...
+    return flux;
+}
+
+bool additionEstSur(unsigned int a, unsigned int b)
+{
+    unsigned int c = a+b;
+    if(c < a || c < b )
+        return false;
+    return true;
+    //return (c < a || c < b) ? false : true;
+    //return !(c < a || c < b)
+}
+
---
+++ entier.hpp
@@ -0,0 +1,60 @@
+#ifndef ENTIER_HPP_INCLUDED
+#define ENTIER_HPP_INCLUDED
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+const unsigned int BASE = 0xffffffff1; //2^32
+
+class Entier
+{
+public:
+    //constructeurs
+    Entier();
+    Entier(unsigned int);
+    Entier(const std::string&);
+    Entier(const Entier&);
+    //destructeur
+    ~Entier();
+    //operateurs
+    //
+    void operator=(const Entier&);
+    void operator=(const int);
+    //ajout
+    Entier& operator+=(const Entier&);
+    //Entier &operator+=(const unsigned int);
+    //soustraire
+    Entier& operator-=(const Entier&);
+    //Entier &operator-=(const unsigned int);
+    //multiplier
+    Entier& operator*=(const Entier&);
+    //Entier &operator*=(const unsigned int);
+    bool estEgal(const Entier&) const;
+    //affichage
+    friend std::ostream& operator<<(std::ostream&, const Entier&);
+    //entrée
+    friend std::istream& operator>>(std::istream&, Entier&);
+
+private:
+    std::vector<unsigned int> valeur;
+};
+
+//operateurs
+Entier operator+(const Entier&, const Entier&);
+//Entier operator+(const Entier&, const unsgned int);
+Entier operator-(const Entier&, const Entier&);
+//Entier operator-(const Entier&, const unsigned int);
+Entier operator*(const Entier&, const Entier&);
+//Entier operator*(const Entier&, const unsigned int);
+//comparaisons
+bool operator<(const Entier&, const Entier&);
+bool operator>(const Entier&, const Entier&);
+bool operator==(const Entier&, const Entier&);
+bool operator!=(const Entier&, const Entier&);
+bool operator<=(const Entier&, const Entier&);
+bool operator>=(const Entier&, const Entier&);
+
+bool additionEstSur(unsigned int a, unsigned int b);
+
+#endif // ENTIER_HPP_INCLUDED
