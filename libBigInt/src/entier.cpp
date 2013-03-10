0ae115fea244f2df8ac9a12af08bd428e7cec7a5
Marc PARDO <p13marc@gmail.com>
2013-03-10 13:38:23 +0100

    zone test minimale et legere modif affichage

--- libBigInt/src/entier.cpp
+++ libBigInt/src/entier.cpp
@@ -263,8 +263,9 @@ Entier operator%(const Entier& a, const Entier& b)
 
 ostream& operator<<(ostream& flux, const Entier& val)
 {
-    for(int i = val.valeur.size()-1; i >= 0; i--)
-        flux << val.valeur[i] << ' ';
+    for(int i = val.valeur.size()-1; i > 0; i--)
+        flux << val.valeur[i] << '.';
+	flux << val.valeur[0];
 
     return flux;
 }
--- src/calculatrice.cpp
+++ src/calculatrice.cpp
@@ -3,13 +3,58 @@
 
 using namespace std;
 
+Entier calcul(const Entier& a, const Entier& b, unsigned char operateur)
+{
+	switch(operateur)
+	{
+		case '+':
+		return a+b;
+		break;
+
+		case '-':
+		return a-b;
+		break;
+
+		case '*':
+		return a*b;
+		break;
+
+		case '/':
+		return a/b;
+		break;
+
+		case '%':
+		return a%b;
+		break;
+	}
+}
+
 int main()
 {
-    Entier a(0xffffffff);
-	cout<<a<<endl;
-	a+=4;
-	cout<<a<<endl;
-	a-=10;
-	cout<<a<<endl;
+	unsigned int choix;
+	Entier op1;
+	Entier op2;
+	unsigned char operateur;
+	
+	do
+	{
+	cout<<"Entrez une ou plusieurs expressions arithmetiques à partir d'un: "<<endl;
+	cout<<"1) clavier		2) fichier"<<endl;
+	cin>>choix;
+	}while(choix != 1 && choix != 2);
+
+	if(choix == 1)
+	{
+		cin>>op1;
+		cin>>operateur;
+		cin>>op2;
+		cout<<op1<<' '<<operateur<<' '<<op2<<" = "<<calcul(op1, op2, operateur)<<endl;
+
+	}
+	else
+	{
+
+	}
+
     return 0;
 }
