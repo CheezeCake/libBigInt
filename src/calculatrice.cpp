f582a55a73699ad57b5afe2a8bb9445a6199e10f
missichini <xd_juju84-xd@hotmail.fr>
2013-03-19 12:20:30 +0100

    Update calculatrice.cpp

--- src/calculatrice.cpp
+++ src/calculatrice.cpp
@@ -1,3 +1,10 @@
+ /**
+    @file calculatrice.cpp
+    @author Marc Pardo
+    @date 2013
+    
+ */
+
 #include <iostream>
 #include <fstream>
 #include "../libBigInt/include/entier.hpp"
@@ -9,35 +16,41 @@ Entier calcul(const Entier& a, const Entier& b, unsigned char operateur)
     switch(operateur)
     {
 	case '+':
-	    return a+b;
+	    return a + b;
 	    break;
 
 	case '-':
-	    return a-b;
+	    return a - b;
 	    break;
 
 	case '*':
-	    return a*b;
+	    return a * b;
 	    break;
 
 	case '/':
-	    return a/b;
+	    return a / b;
 	    break;
 
 	case '%':
-	    return a%b;
+	    return a % b;
 	    break;
     }
 }
 
 bool comparer(const Entier& a, const Entier& b, string operateur)
 {
-    if(operateur == "<")return a<b;
-    if(operateur == ">")return a>b;
-    if(operateur == "<=")return a<=b;
-    if(operateur == ">=")return a>=b;
-    if(operateur == "=")return a==b;
-    if(operateur == "!=")return a!=b;
+    if(operateur == "<")
+    return a < b;
+    if(operateur == ">")
+    return a > b;
+    if(operateur == "<=")
+    return a <= b;
+    if(operateur == ">=")
+    return a >= b;
+    if(operateur == "=")
+    return a == b;
+    if(operateur == "!=")
+    return a != b;
 }
 
 int main()
@@ -50,21 +63,22 @@ int main()
 
     do
     {
-	cout<<"Entrez une ou plusieurs expressions arithmetiques à partir d'un: "<<endl;
-	cout<<"1) clavier		2) fichier"<<endl;
+	cout <<"Entrez une ou plusieurs expressions arithmetiques à partir d'un: "<< endl;
+	cout <<"1) clavier		2) fichier"<< endl;
 	cin>>choix;
-    }while(choix != 1 && choix != 2);
+    }
+    while(choix != 1 && choix != 2);
 
     if(choix == 1)
     {
-	cout<<"Entrez une suite d'expression"<<endl;
+	cout <<"Entrez une suite d'expression"<< endl;
 	//cout << "Entrez la premiere operande :" << endl;
-	cin>>op1;
+	cin >> op1;
 	//cout << endl << "Entrez l'operateur :" << endl;
-	cin>>operateur;
+	cin >> operateur;
 	//cout << endl << "Entrez la seconde operande :" << endl;
-	cin>>op2;
-	cout << endl <<op1<<' '<<operateur<<' '<<op2<<" = "<<calcul(op1, op2, operateur)<<endl;
+	cin >> op2;
+	cout << endl << op1 <<' '<< operateur <<' '<< op2 <<" = "<< calcul(op1, op2, operateur) << endl;
 
     }
     else
