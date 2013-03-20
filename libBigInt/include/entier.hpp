03cf9670d5f9e535e79ddef94832bce68fa1587f
JulienAJ <gammer84@hotmail.fr>
2013-03-20 14:56:12 +0100

    Update entier.hpp

--- libBigInt/include/entier.hpp
+++ libBigInt/include/entier.hpp
@@ -1,7 +1,8 @@
 /**
-    @file entier.hpp
-    @author Marc Pardo,Emmanuel Nicolet
-    @date 2013
+    \file entier.hpp
+    \brief Librairie d'opérations sur grands entiers
+    \author Marc Pardo, Emmanuel Nicolet, Alonso Jimenez Julien, Missichini Julien
+    \date Mars 2013
     
 */
 
@@ -15,16 +16,53 @@
 
 const unsigned long long int BASE = 0x100000000; //2^32
 
+/* \class Entier
+ * \brief classe représentant le nombre
+ * La classe gère un entier de grnade taille
+ */
+ 
 class Entier
 {
 public:
+
     //constructeurs
     Entier();
-    Entier(unsigned int);
-    Entier(const std::string&, unsigned int base = 10);
+    /*!
+     *  \brief Constructeur à partir d'un entier
+     *
+     *  Constructeur de la classe Entier
+     *
+     *  \param val : Entier en base 10 à convertir
+     */
+    Entier(unsigned int val);
+    /*!
+     *  \brief Constructeur à partir d'une chaine de caractères 
+     *
+     *  Constructeur de la classe Entier
+     *
+     *  \param nombre : Chaine contenant l'entier
+     *  \param base : Base de l'entier dans la chaine nombre (10 par défault)
+     */
+    Entier(const std::string& nombre, unsigned int base = 10);
+    /*!
+     *  \brief Constructeur à partir d'un objet de la classe Entier 
+     *
+     *  Constructeur de la classe Entier
+     *
+     *  \param val : Entier source
+     */
     Entier(const Entier&);
+    
+    
     //destructeur
+    /*!
+     *  \brief Destructeur
+     *
+     *  Destructeur de la classe Entier
+     */
     ~Entier();
+    
+    
     //modulateurs
     bool set(const std::string&, unsigned int base = 10);
     //operateurs
@@ -60,7 +98,7 @@ public:
     void shrink_to_fit();
 
 private:
-    std::vector<unsigned int> valeur;
+    std::vector<unsigned int> valeur; /*!< Nombre décomposé en valeurs de base 2^32*/
 	void mul(long long unsigned int a, long long unsigned int b, unsigned int& retenue, unsigned int& reste);
 };
 
